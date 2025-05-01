78342c2a285f3363d56c76662ad9aadf34a4f2e6
CPMTooldev <telmnbattr@gmail.com>
2025-05-01 17:58:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def load_player_data(cpm):
             
         else:
             print(Colorate.Horizontal(Colors.rainbow, '! ERROR: new accounts most be signed-in to the game at least once !.'))
-            exit(1)
+            sleep(1)
     else:
         print(Colorate.Horizontal(Colors.rainbow, '! ERROR: seems like your login is not properly set !.'))
         exit(1)
